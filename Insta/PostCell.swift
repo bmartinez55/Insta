2c43639bf23f33e0d23ce23b20b6e24435498db3
Brian Martinez <bmartinez55@brians-mac.local>
2019-03-03 22:56:30 -0800

    initial Commit

---
+++ Insta/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Insta
+//
+//  Created by Brian Martinez on 2/27/19.
+//  Copyright © 2019 Brian Martinez. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
