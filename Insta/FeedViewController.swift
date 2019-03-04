161b330ccccb97e43711f5882919e515c613b096
Brian Martinez <bmartinez55@brians-mac.local>
2019-03-04 14:34:01 -0800

    Log out feature implemented

--- Insta/FeedViewController.swift
+++ Insta/FeedViewController.swift
@@ -69,5 +69,17 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func logoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    
+    
 }
